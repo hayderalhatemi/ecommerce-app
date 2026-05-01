ad4d4d9ff601fb5eda9c884026498d8639caf565
Hayder <hayder.alhatemi@gmail.com>
2026-05-01 12:37:14 +0300

    feat: add backend src structure and types

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,9 @@
+import { jwtPayload } from 'jsonwebtoken';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: jwtPayload & { id: string, role: string };
+        }
+    }
+}
