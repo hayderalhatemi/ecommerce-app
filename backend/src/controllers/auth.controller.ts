d86ac51b0c67889df6bc5448bd6f8fe00d8b8767
Hayder <hayder.alhatemi@gmail.com>
2026-05-09 20:01:52 +0300

    feat: add register controller logic

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,3 +2,18 @@ import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model';
 import { RegisterInput, LoginInput } from '../schemas/auth.schema';
+
+// Generate jwt token
+const generateToken = (id: string, role: string): => {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET as string, { expiresIn: '7d'});
+};
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+    const { name, email, password } = req.body as RegisterInput;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        res.status(400).json({ message: 'Email already in use' });
+        return;
+    }
+}
