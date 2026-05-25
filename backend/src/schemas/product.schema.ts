1f7768b1c4c572ea8390b4e5a20eb80805f23018
Hayder <hayder.alhatemi@gmail.com>
2026-05-25 22:47:20 +0300

    fixed product.schema.ts typos

--- backend/src/schemas/product.schema.ts
+++ backend/src/schemas/product.schema.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod';
 
 export const createProductSchema = z.object({
-    body: z.object({
-        name: z.string().min(2, 'Name must be at least 2 charcters'),
-        description: z.string().min(10, 'Description must be at least 10 charecters'),
-        price: z.coerce.number().positive('Price must be a positive number'),
-        category: z.coerce.number().int().nonnegative('Stock cannot be negative'),
-    }),
+  body: z.object({
+    name: z.string().min(2, 'Name must be at least 2 characters'),
+    description: z.string().min(10, 'Description must be at least 10 characters'),
+    price: z.coerce.number().positive('Price must be a positive number'),
+    category: z.string().min(2, 'Category is required'),
+    stock: z.coerce.number().int().nonnegative('Stock cannot be negative'),
+  }),
 });
 
 export const updateProductSchema = z.object({
@@ -20,4 +21,4 @@ export const updateProductSchema = z.object({
 });
 
 export type CreateProductInput = z.infer<typeof createProductSchema>['body'];
-export type updateProductInput = z.infer<typeof updateProductSchema>['body'];
+export type UpdateProductInput = z.infer<typeof updateProductSchema>['body'];
