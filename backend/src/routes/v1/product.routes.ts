1619cf35f47b54f7331a68225192779987915823
Hayder <hayder.alhatemi@gmail.com>
2026-05-29 22:21:33 +0300

    feat: add product upload middleware integration

---
+++ backend/src/routes/v1/product.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from '../../controllers/product.controller';
+import { protect, restrictTo } from '../../middlewares/auth.middleware';
+import validate from '../../middlewares/validate.middleware';
+import { createProductSchema, updateProductSchema } from '../../schemas/product.schema';
+import upload from '../../middlewares/upload.middleware';
