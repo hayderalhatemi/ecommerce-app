47f6bb84621d5e6381d7f4faee093903b92851c1
Hayder <hayder.alhatemi@gmail.com>
2026-06-14 22:38:31 +0300

    feat: add order zod schema

--- backend/src/schemas/order.schema.ts
+++ backend/src/schemas/order.schema.ts
@@ -7,3 +7,17 @@ const orderItemSchema = z.object({
     quantity: z.number().int().positive('Quantitiy must be at least 1'),
     image: z.string().optional(),
 });
+
+export const createOrderSchema = z.object({
+    body: z.object({
+        items: z.array(orderItemSchema).min(1, 'Order must have at least one item'),
+        shippingAddress: z.object({
+            address: z.string().min(1, 'Address is required'),
+            city: z.string().min(1, 'City is required'),
+            postalCode: z.string().min(1, 'Postal code is required'),
+            country: z.string().min(1, 'Country is required')
+        }),
+    }),
+});
+
+export type CreateOrderInput = z.infer<typeof createOrderSchema>['body'];
