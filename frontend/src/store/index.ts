d09c2d17e401b2dab9783fac7e8a872e9a95876e
Hayder <hayder.alhatemi@gmail.com>
2026-07-07 16:23:15 +0300

    feat: add redux store

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import cartReducer from './slices/cartSlice';
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        cart: cartReducer,
+    },
+});
+
+// Inferred types for use throughout the app
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispath = typeof store.dispatch;
+
+export default store;
