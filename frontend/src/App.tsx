a83f1347ecbc86f9d72eae93020601f0f7a08867
Hayder <hayder.alhatemi@gmail.com>
2026-07-17 15:21:26 +0300

    feat: add cart route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<div>Cart Page</div>} />
+          <Route path="/cart" element={<CartPage />} />
           <Route path="/orders" element={<div>Orders Page</div>} />
           <Route path="/admin" element={<div>Admin Dashboard</div>} />
         </Routes>
