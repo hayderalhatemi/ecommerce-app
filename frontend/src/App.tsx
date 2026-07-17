5807cbeae2256c0ddd6e4c5a36fea84b02a46e90
Hayder <hayder.alhatemi@gmail.com>
2026-07-17 11:05:39 +0300

    feat: connect product details route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/products/:id" element={<div>Product Detail Page</div>} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/cart" element={<div>Cart Page</div>} />
           <Route path="/orders" element={<div>Orders Page</div>} />
           <Route path="/admin" element={<div>Admin Dashboard</div>} />
